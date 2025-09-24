81bbbed520384cb2a1b908f1744e09ee147c7936
Eduardo de Andrade do Bomfim Júnior <166822209+Eduardo-Bomfim@users.noreply.github.com>
2025-09-24 18:27:00 -0300

    ✨ feat: adiciona o Flask e altera o prompt_system

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 from models.Chatbot import Chatbot
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 print("Flask app initialized.")
 
 try:
