d5a0a48e8f21e7977398458395483050df57d2c7
Eduardo de Andrade do Bomfim Júnior <166822209+Eduardo-Bomfim@users.noreply.github.com>
2025-09-24 18:22:49 -0300

    ✨ feat(chat): adiciona a interface completa com renderização de roteiros

---
+++ frontend/src/pages/Chat/ChatPage.tsx
@@ -0,0 +1,9 @@
+import { Chat } from "../../components/Chat/Chat";
+
+export default function ChatPage() {
+    return (
+        <div className="app-container">
+            <Chat />
+        </div>
+    )
+}
